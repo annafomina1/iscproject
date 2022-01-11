a9a0ea9bdef41559ceb9348904b933f92c5000fb
HarperRapkin <92805074+HarperRapkin@users.noreply.github.com>
2022-01-11 17:18:50 -0500

    Create LoginView.java
    added the LoginView (login page)

---
+++ src/main/java/com/example/application/views/login/LoginView.java
@@ -0,0 +1,46 @@
+package com.example.application.views.login;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
+import com.example.application.data.service.AuthService;
+import com.example.application.data.service.UserRepository;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.notification.Notification;
+
+@PageTitle("Login Form")
+@Route(value="login", layout=MainLayout.class)
+public class LoginView extends VerticalLayout{
+
+    AuthService authService;
+
+    public LoginView(AuthService authService){
+        this.authService = authService;
+        setId("login-view");
+        var username = new TextField("Username");
+        var password = new PasswordField("Password");
+        add(
+                new com.vaadin.flow.component.Component[]{new H1("login"), username, password, new Button("Login", event -> {
+
+                    try {
+                        //if the login is successful
+                        authService.authenticate(username.getValue(), password.getValue());
+                        Notification notification = Notification.show("Login Successful");
+                        username.clear();
+                        password.clear();
+                        
+                    } catch (AuthService.AuthException e) {
+                        //if the login is unsuccessful
+                        Notification notification = Notification.show("Login Unsuccessful");
+                        username.clear();
+                        password.clear();
+                    }
+
+                })});
+    }
+}
