717d5da8ce589fa8c9ac78fe2bba17fbe7a6bc10
HarperRapkin <92805074+HarperRapkin@users.noreply.github.com>
2022-01-11 17:24:07 -0500

    Update UserRepository.java
    added get by username

--- src/main/java/com/example/application/data/service/UserRepository.java
+++ src/main/java/com/example/application/data/service/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDate;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+  User getByUsername(String username);
 }
