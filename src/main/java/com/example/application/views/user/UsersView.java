00dc6b2719f3890898069209abec95ed8cafd4f3
HarperRapkin <92805074+HarperRapkin@users.noreply.github.com>
2022-01-26 16:40:47 -0500

    added import statement

--- src/main/java/com/example/application/views/user/UsersView.java
+++ src/main/java/com/example/application/views/user/UsersView.java
@@ -11,6 +11,7 @@ import java.util.List;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 
 
 @PageTitle("User List")
