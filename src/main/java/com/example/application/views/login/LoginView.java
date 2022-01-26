0bdc64aa361c66f0f977d86613328298ce21a28d
HarperRapkin <92805074+HarperRapkin@users.noreply.github.com>
2022-01-26 14:55:55 -0500

    Added help pop-up window

--- src/main/java/com/example/application/views/login/LoginView.java
+++ src/main/java/com/example/application/views/login/LoginView.java
@@ -10,12 +10,18 @@ import com.vaadin.flow.component.html.H1;
 import com.example.application.data.service.AuthService;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.Text;
 
 @PageTitle("Login Form")
 @Route(value="login", layout=MainLayout.class)
 public class LoginView extends VerticalLayout{
 
     AuthService authService;
+    
+    private Button help = new Button("?");
+    private Button closeButton = new Button("x");
+    private Notification notification = new Notification();
+    private Text text = new Text ("If you have made an account, login here.");
 
     public LoginView(AuthService authService){
         this.authService = authService;
@@ -40,5 +46,19 @@ public class LoginView extends VerticalLayout{
                     }
 
                 })});
+        add(help);
+
+        Button closeButton = new Button("x");
+        closeButton.getElement().setAttribute("aria-label", "Close");
+        closeButton.addClickListener(event -> {
+            notification.close();
+        });
+
+        help.addClickListener(e ->{
+            HorizontalLayout layout = new HorizontalLayout(text, closeButton);
+            notification.add(layout);
+            notification.open();
+
+        });
     }
 }
