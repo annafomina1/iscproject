367e50003f632f9d3c0ff883e6b88e7e38bbea6d
avadonaldson <avalaine18@gmail.com>
2022-01-27 10:30:50 -0500

    Added some commenting

--- src/main/java/com/example/application/views/home/HomePage.java
+++ src/main/java/com/example/application/views/home/HomePage.java
@@ -1,5 +1,6 @@
 package com.example.application.views.home;
 
+//imports
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -7,20 +8,33 @@ import com.vaadin.flow.router.PageTitle;
 import com.example.application.views.MainLayout;
 import com.vaadin.flow.router.RouteAlias;
 
+/*
+The Home Page
+Created By: Ava Donaldson
+ */
 
+//page title and route
 @PageTitle("Home")
 @Route(value = "home", layout = MainLayout.class)
 @RouteAlias(value = "", layout = MainLayout.class)
+
+//HomePage
 public class HomePage extends VerticalLayout {
 
     public HomePage() {
         setSizeFull();
 
+        //gets the image of the skibble logo
         Image img = new Image("images/skibble-logo.png", "skibble logo");
+
+        //sets image sizing
         img.setWidth("400px");
         img.setHeight("300px");
+
+        //adds the image to the view
         add(img);
 
+        //sets size and aligns page
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
@@ -28,4 +42,4 @@ public class HomePage extends VerticalLayout {
     }
 
 
-}
+}//end
