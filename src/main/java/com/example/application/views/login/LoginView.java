d456f41582cd36f01f49cb89c14d523697414c6d
HarperRapkin <92805074+HarperRapkin@users.noreply.github.com>
2022-01-27 09:07:13 -0500

    added commenting

--- src/main/java/com/example/application/views/login/LoginView.java
+++ src/main/java/com/example/application/views/login/LoginView.java
@@ -14,22 +14,38 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 
+/*
+The page view for the login form, gets username and password from the user and uses AuthService to auntheticate their input
+Created By: Harper Rapkin
+Date Created: 04/01/2022
+Date Last Edited: 04/01/2022
+ */
 @PageTitle("Login Form")
 @Route(value="login", layout=MainLayout.class)
 public class LoginView extends VerticalLayout{
 
+    //creating an AuthService object
     AuthService authService;
     
-    private Button help = new Button("?");
-    private Button closeButton = new Button("x");
-    private Notification notification = new Notification();
-    private Text text = new Text ("If you have made an account, login here.");
+    //definning the fields
+    private Button help = new Button("?");//help button
+    private Button closeButton = new Button("x");//close for the help notification
+    private Notification notification = new Notification();//creating a notification object
+    private Text text = new Text ("If you have made an account, login here.");//the text that appears on the popup window
 
+    /**
+    LoginView constructor that connects to AuthService, authenticates the user, and has option to show help notification
+    @param authService the AuthService
+    */
     public LoginView(AuthService authService){
+        //defining the authService
         this.authService = authService;
         setId("login-view");
+        
+        //creating the objects for the username and password form
         var username = new TextField("Username");
         var password = new PasswordField("Password");
+        //adding a header, the username and password fillable elements, and a button to login
         add(
                 new com.vaadin.flow.component.Component[]{new H1("Login"), username, password, new Button("Login", event -> {
 
@@ -48,14 +64,17 @@ public class LoginView extends VerticalLayout{
                     }
 
                 })});
+        //adding the button to access the help popup window
         add(help);
 
+        //setting the attributes and actions for the button
         Button closeButton = new Button("x");
         closeButton.getElement().setAttribute("aria-label", "Close");
         closeButton.addClickListener(event -> {
             notification.close();
         });
 
+        //setting the attributes and actions for the button, adding the notification when the button is clicked
         help.addClickListener(e ->{
             HorizontalLayout layout = new HorizontalLayout(text, closeButton);
             notification.add(layout);
